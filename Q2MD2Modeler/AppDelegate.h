28f9fc7495c07665b857ad5a132c32c8d5f14d70
Tanoy Sinha <tsinha@gmail.com>
2012-10-04 18:41:02 -0400

    Initial Commit

---
+++ Q2MD2Modeler/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Q2MD2Modeler
+//
+//  Created by Tanoy Sinha on 10/4/12.
+//  Copyright (c) 2012 Tanoy Sinha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Q2MD2Modeler/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Q2MD2Modeler
+//
+//  Created by Tanoy Sinha on 10/4/12.
+//  Copyright (c) 2012 Tanoy Sinha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
